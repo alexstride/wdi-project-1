c6a00b387cc4daba1cb947b70760cd6004404e97
alexstride <stridealexander@gmail.com>
2017-08-27 20:03:20 +0100

    Bugs gone. Code refactored to be more object-oriented. Event now fired when user requests a valid move

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,8 @@ const gridObject = {
   height: HEIGHT,
   valueArray: null, //2D array of number values, representing colors
   $elementArray: null, //1D array of .box elements on the page
+  activeBox1: null,
+  activeBox2: null,
 
   initializeValueArray: function () {
     //A function to create a new grid object (array of arrays) using the dimensions defined in args.
@@ -40,83 +42,55 @@ const gridObject = {
     return this.valueArray.reduce((flatArray, array) => flatArray.concat(array), []);
   }
 
-};
+}; //End of the gridObject declaration
 
+//Creating a Move object, which can be created for each move the user wants to make.
+const moveObject = {
+  activeBox1: null,
+  activeBox2: null,
+  areAdjacent: function() {
+    return (Math.abs($(this.activeBox1).data('x') - $(this.activeBox2).data('x')) + Math.abs($(this.activeBox1).data('y') - $(this.activeBox2).data('y'))) === 1;
+  },
+
+  triggerMoveEvent: function() {
+    //notify the grid object that there is now a change that needs to be handled and send through the coordinates of the squares which are being moved.
+    $(document).trigger('userMove', [{x: $(this.activeBox1).data('x'), y: $(this.activeBox1).data('y')}, {x: $(this.activeBox2).data('x'), y: $(this.activeBox2).data('y')}]);
+
+  },
+  deactivateBoxes: function() {
+    //Using a setTimeout of 0 to ensure that this stage cannot be queued to happen before the information about the clicked squares has been read from the DOM.
+    window.setTimeout(() => {
+      $(this.activeBox1).text('');
+      this.activeBox1 = null;
+      $(this.activeBox2).text('');
+      this.activeBox2 = null;
+    }, 0);
+  }
+}; //End of the moveObject
+
+//This function needs to sit in the global scope as it inherits its context from the click event.
+const processClick = function(clickEvent) {
+  console.log(moveObject.activeBox1);
+  if (!moveObject.activeBox1) {
+    console.log('Setting active box 1');
+    moveObject.activeBox1 = clickEvent.target;
+    $(clickEvent.target).text('CSS');
+  } else {
+    moveObject.activeBox2 = clickEvent.target;
+    if (moveObject.areAdjacent()) {
+      moveObject.triggerMoveEvent();
+    }
+    moveObject.deactivateBoxes();
+  }
+
+};
 
 $(function() {
   gridObject.initializeValueArray();
   gridObject.initializeElementArray();
   gridObject.initializeColors();
-  gridObject.$elementArray.on('click', () => {
-    console.log('You\'ve clicked on an element!');
+  gridObject.$elementArray.on('click', processClick);
+  $(document).on('userMove', (e, pos1, pos2) => {
+    console.log('User wants to move', pos1, pos2);
   });
 });
-
-// //Functions pertaining to selecting pairs of squares in the grid
-//
-// function areAdjacent($square1, $square2) {
-//   //Takes in two squares (as jquery objects) and returns boolean indicating whether they are adjacent.
-//   return (Math.abs($square1.data('x') - $square2.data('x')) + Math.abs($square1.data('y') - $square2.data('y'))) === 1;
-// }
-//
-// function selectSquare($square) {
-//   //Takes in a square as a jquery object, changes some styles on it and returns the same element passed in.
-//   console.log(`selecting square: (${$square.data('x')}, ${$square.data('y')})`);
-//   $square.text('!!');
-//   return $square;
-// }
-//
-// function deselectSquare($square) {
-//   console.log(`deselecting square: (${$square.data('x')}, ${$square.data('y')})`);
-//   $square.text('');
-//   return $square;
-// }
-//
-// $(() => {
-//   function fillColors() {
-//     //A function to initally fill all of the boxes with random colors
-//     //flatenning out grid object
-//     const flatGridArray = gridObject.returnFlat();
-//     //looping over the dom elements for the boxes applyng the colors
-//     for (let i = 0; i < $gridBoxes.length; i++) {
-//       gridObject.fillByHTML($gridBoxes[i], flatGridArray[i]);
-//     }
-//   }
-//
-//   //INITIAL SETUP
-//   //create a new grid object in memory
-//   gridObject.initializeValueArray();
-//   //grab all of the grid boxes
-//   const $gridBoxes = $('.box');
-//   //fill all of the gridBoxes with their corresponding colors from the gridObject
-//   fillColors();
-//   //TO DO: Use a match-checking function to eliminate any matches which are already in the grid at ititialization and bring in new random elements.
-//   //END OF INITIAL SETUP
-//
-//   //Placing click event on all of the boxes which will register the coordinates of the two boxes clicked.
-//
-//   //Two variables to store the pair of elements clicked on.
-//   let $clickedSquare1 = null;
-//   let $clickedSquare2 = null;
-//   $gridBoxes.on('click', function(e) {
-//     const a = new Date();
-//     console.log(`click detected: ${a.getSeconds()}.${a.getMilliseconds()}`,$(e.target).data());
-//     // if(!$clickedSquare1) {
-//     //   $clickedSquare1 = $(e.target);
-//     //   selectSquare($clickedSquare1);
-//     // } else if (!$clickedSquare2) {
-//     //   $clickedSquare2 = $(e.target);
-//     //   selectSquare($clickedSquare2);
-//     //   //run a function to check whether the two elements are adjacent. Logs and resets them if they are
-//     //   if(areAdjacent($clickedSquare1, $clickedSquare2)) {
-//     //     console.log($clickedSquare1.data(), $clickedSquare2.data());
-//     //   }
-//     //   //deselectSquare($clickedSquare1);
-//     //   //$clickedSquare1 = null;
-//     //   //deselectSquare($clickedSquare2);
-//     //   //$clickedSquare2 = null;
-//     // } else {
-//     //   console.log('Both active squares have been set');
-//     // }
-//   });
-// });
