65d573728540a3448ac168ca017edbf4f0297790
alexstride <stridealexander@gmail.com>
2017-08-27 13:18:02 +0100

    gridObject created and many functions subsumed within. Bug exists in click events for square selection

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,41 @@
-const colorArray = ['#5679f7', '#f78a0e', '#d6d6d6', '#d60202', '#8f14d1', '#13c600', '#fcf63c'];
+const COLORS = ['#5679f7', '#f78a0e', '#d6d6d6', '#d60202', '#8f14d1', '#13c600', '#fcf63c'];
 const WIDTH = 8;
+const HEIGHT = WIDTH;
 
-function createNewGridObject(width, numColors) {
-  //A function to create a new grid object (array of arrays) using the dimensions defined by parameter n.
-  const resultArray = [];
-  for (let i = 0; i < width; i++) {
-    const row = [];
-    for (let j = 0; j < width; j++) {
-      row.push(Math.floor(Math.random()*numColors));
+const gridObject = {
+  colorArray: COLORS,
+  width: WIDTH,
+  height: HEIGHT,
+  valueArray: null, //this.initializeValueArray(this.width, this.height, colorArray.length)
+
+  initializeValueArray: function () {
+    //A function to create a new grid object (array of arrays) using the dimensions defined in args.
+    const resultArray = [];
+    for (let i = 0; i < this.height; i++) {
+      const row = [];
+      for (let j = 0; j < this.width; j++) {
+        row.push(Math.floor(Math.random()*this.colorArray.length));
+      }
+      resultArray.push(row);
     }
-    resultArray.push(row);
-  }
-  return resultArray;
-}
+    this.valueArray = resultArray;
+  },
 
-function fillFunction(element, number) {
-  //Takes an html element and fills it with a color as inicated by an integer (looked up in the color array)
-  $(element).css('background-color', colorArray[number]);
-}
+  returnFlat: function() {
+    return this.valueArray.reduce((flatArray, array) => flatArray.concat(array), []);
+  },
+
+  fillByHTML: function(element, number) {
+    //Takes an html element and fills it with a color as inicated by an integer (looked up in the color array)
+    $(element).css('background-color', this.colorArray[number]);
+  }
+
+};
+
+
+
+
+//Functions pertaining to selecting pairs of squares in the grid
 
 function areAdjacent($square1, $square2) {
   //Takes in two squares (as jquery objects) and returns boolean indicating whether they are adjacent.
@@ -26,29 +44,31 @@ function areAdjacent($square1, $square2) {
 
 function selectSquare($square) {
   //Takes in a square as a jquery object, changes some styles on it and returns the same element passed in.
+  console.log(`selecting square: (${$square.data('x')}, ${$square.data('y')})`);
   $square.text('!!');
   return $square;
 }
 
 function deselectSquare($square) {
+  console.log(`deselecting square: (${$square.data('x')}, ${$square.data('y')})`);
   $square.text('');
   return $square;
 }
 
 $(() => {
   function fillColors() {
-    //A function to initally fill all of the boxes with random colors;
+    //A function to initally fill all of the boxes with random colors
     //flatenning out grid object
-    const flatGridArray = gameObject.reduce((flatArray, array) => flatArray.concat(array));
+    const flatGridArray = gridObject.returnFlat();
     //looping over the dom elements for the boxes applyng the colors
     for (let i = 0; i < $gridBoxes.length; i++) {
-      fillFunction($gridBoxes[i], flatGridArray[i]);
+      gridObject.fillByHTML($gridBoxes[i], flatGridArray[i]);
     }
   }
 
   //INITIAL SETUP
   //create a new grid object in memory
-  const gameObject = createNewGridObject(WIDTH, colorArray.length);
+  gridObject.initializeValueArray();
   //grab all of the grid boxes
   const $gridBoxes = $('div.box');
   //fill all of the gridBoxes with their corresponding colors from the gridObject
@@ -63,16 +83,18 @@ $(() => {
   let $clickedSquare2 = null;
   $gridBoxes.on('click', function(e) {
     if(!$clickedSquare1) {
-      $clickedSquare1 = selectSquare($(e.target));
+      $clickedSquare1 = $(e.target);
+      selectSquare($clickedSquare1);
     } else {
-      $clickedSquare2 = selectSquare($(e.target));
+      $clickedSquare2 = $(e.target);
+      selectSquare($clickedSquare2);
       //run a function to check whether the two elements are adjacent. Logs and resets them if they are
       if(areAdjacent($clickedSquare1, $clickedSquare2)) {
         console.log($clickedSquare1.data(), $clickedSquare2.data());
       }
-      deselectSquare($clickedSquare1);
+      //deselectSquare($clickedSquare1);
       $clickedSquare1 = null;
-      deselectSquare($clickedSquare2);
+      //deselectSquare($clickedSquare2);
       $clickedSquare2 = null;
     }
   });
